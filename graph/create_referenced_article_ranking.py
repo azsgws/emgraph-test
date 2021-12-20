40b3ba5c0f12987b06312b2bcc383819a140a96f
w028ff <a085vgu@yamaguchi-u.ac.jp>
2021-12-20 14:53:42 +0900

    create referenced article ranking

--- graph/create_referenced_article_ranking.py
+++ graph/create_referenced_article_ranking.py
@@ -67,6 +67,56 @@ def create_table_pagerank_auth_referenced_articles(mizar_ver):
             f.write("| " + article2pagerank[i][0] + " | " + article2authority[i][0] + " | " + article2number_of_referenced[i][0] + " |\n")
 
 
+def make_article2authority_minus_pagerank(mml_ver):
+    cwd = os.getcwd()
+    try:
+        os.chdir("graph_attrs")
+        with open("dot_graph_" + mml_ver + "_authority_minus_pagerank.json", "r") as f:
+            graph = json.load(f)
+    finally:
+        os.chdir(cwd)
+     # networkxのグラフを作成
+    G = nx.cytoscape_graph(graph)
+
+    return nx.get_node_attributes(G, "auth_minus_pagerank")
+
+    
+
+def create_article2pagerank_authority_referenced_ranking(mml_ver):
+    with open("research_data/article2number_of_referenced("+ mml_ver +").json", "r") as f:
+        article2number_of_referenced = json.load(f)
+    article2number_of_referenced = sorted(article2number_of_referenced.items(), key=lambda x:x[1], reverse=True)
+    article2authority = sorted(make_article2authority_from_graph_attrs(mml_ver).items(), key=lambda x:x[1], reverse=True)
+    article2pagerank = sorted(make_article2pagerank_from_graph_attrs(mml_ver).items(), key=lambda x:x[1], reverse=True)
+    article2authortiy_minus_pagerank = sorted(make_article2authority_minus_pagerank(mml_ver).items(), key=lambda x:x[1], reverse=False)
+
+    article2number_of_referenced_ranking = dict()
+    article2authority_ranking = dict()
+    article2pagerank_ranking = dict()
+    article2authortiy_minus_pagerank_ranking = dict()
+
+    for i in range(len(article2number_of_referenced)):
+        article2number_of_referenced_ranking[article2number_of_referenced[i][0]] = i + 1
+    for i in range(len(article2authority)):
+        article2authority_ranking[article2authority[i][0]] = i + 1
+    for i in range(len(article2pagerank)):
+        article2pagerank_ranking[article2pagerank[i][0]] = i + 1
+    for i in range(len(article2authortiy_minus_pagerank)):
+        article2authortiy_minus_pagerank_ranking[article2authortiy_minus_pagerank[i][0]] = i + 1
+
+
+    with open("research_data/article2pagerank_authority_number_of_articles_ranking.md", "w") as f:
+        f.write("## article to ranking\n")
+        f.write("| article | referenced articles | pagerank | authority | Authority - PageRank(ascending) | \n")
+        f.write("| ------- | ------------------- | -------- | --------- |-------------------------------- | \n")
+        for k in article2number_of_referenced_ranking.keys():
+            f.write("| " + str(k) + 
+                    " | " + str(article2number_of_referenced_ranking[k]) + 
+                    " | " + str(article2pagerank_ranking[k]) + 
+                    " | " + str(article2authority_ranking[k]) + 
+                    " | " + str(article2authortiy_minus_pagerank_ranking[k]) +" |\n")
+
 if __name__ == '__main__':
     create_article2number_of_referenced("2020-06-18", create_file=True)
     create_table_pagerank_auth_referenced_articles("2020-06-18")
+    create_article2pagerank_authority_referenced_ranking("2020-06-18")
