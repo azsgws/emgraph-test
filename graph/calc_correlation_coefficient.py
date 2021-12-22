7500d52fb37c267c2dafd7b472964861c333aa2e
w028ff <a085vgu@yamaguchi-u.ac.jp>
2021-12-22 19:35:37 +0900

    相関係数を計算: theorem,defとAuth-PR, Auth, PR

--- graph/calc_correlation_coefficient.py
+++ graph/calc_correlation_coefficient.py
@@ -60,7 +60,7 @@ def calc_correlation_coefficient_number_of_referenced_and_authority():
     return res
 
 def calc_correlation_coefficient_number_of_labels_and_authortiy_minus_pagerank():
-    with open("article2number_of_theorems_or_defintions.json", "r") as f:
+    with open("article2number_of_labels.json", "r") as f:
         article2number_of_theorems_or_definitons = json.load(f)
     article2authortiy_minus_pagerank = make_article2authority_minus_pagerank("2020-06-18")
     number_of_labels = list()
@@ -78,7 +78,7 @@ def calc_correlation_coefficient_number_of_labels_and_authortiy_minus_pagerank()
     return res
 
 def calc_correlation_coefficient_number_of_labels_and_pagerank():
-    with open("article2number_of_theorems_or_defintions.json", "r") as f:
+    with open("article2number_of_labels.json", "r") as f:
         article2number_of_theorems_or_definitons = json.load(f)
     article2authortiy_minus_pagerank = make_article2pagerank_from_graph_attrs("2020-06-18")
     number_of_labels = list()
@@ -96,7 +96,7 @@ def calc_correlation_coefficient_number_of_labels_and_pagerank():
     return res
 
 def calc_correlation_coefficient_number_of_labels_and_authortiy():
-    with open("article2number_of_theorems_or_defintions.json", "r") as f:
+    with open("article2number_of_labels.json", "r") as f:
         article2number_of_theorems_or_definitons = json.load(f)
     article2authortiy = make_article2authority_from_graph_attrs("2020-06-18")
     number_of_labels = list()
@@ -113,6 +113,60 @@ def calc_correlation_coefficient_number_of_labels_and_authortiy():
 
     return res
 
+def calc_correlation_coefficient_number_of_theorem_and_definiton_between_authoritiy_minus_pagerank():
+    with open("article2number_of_theorems_or_defintions.json", "r") as f:
+        article2number_of_theorems_or_definitons = json.load(f)
+    article2authortiy_minus_pagerank = make_article2authority_minus_pagerank("2020-06-18")
+    number_of_theorems_and_definitions = list()
+    authority_minus_pagerank = list()
+    for k, v in article2number_of_theorems_or_definitons.items():
+        key = re.sub(r"\.miz", "", k).upper()
+        number_of_theorems_and_definitions.append(float(v))
+        authority_minus_pagerank.append(float(article2authortiy_minus_pagerank[key]))
+        
+    s1 = pd.Series(number_of_theorems_and_definitions)
+    s2 = pd.Series(authority_minus_pagerank)
+
+    res = s1.corr(s2)
+
+    return res
+
+def calc_correlation_coefficient_number_of_theorem_and_definiton_between_pagerank():
+    with open("article2number_of_theorems_or_defintions.json", "r") as f:
+        article2number_of_theorems_or_definitons = json.load(f)
+    article2pagerank = make_article2pagerank_from_graph_attrs("2020-06-18")
+    number_of_theorems_and_definitions = list()
+    pagerank = list()
+    for k, v in article2number_of_theorems_or_definitons.items():
+        key = re.sub(r"\.miz", "", k).upper()
+        number_of_theorems_and_definitions.append(float(v))
+        pagerank.append(float(article2pagerank[key]))
+        
+    s1 = pd.Series(number_of_theorems_and_definitions)
+    s2 = pd.Series(pagerank)
+
+    res = s1.corr(s2)
+
+    return res
+
+def calc_correlation_coefficient_number_of_theorem_and_definiton_between_authoritiy():
+    with open("article2number_of_theorems_or_defintions.json", "r") as f:
+        article2number_of_theorems_or_definitons = json.load(f)
+    article2authortiy = make_article2authority_from_graph_attrs("2020-06-18")
+    number_of_theorems_and_definitions = list()
+    authority = list()
+    for k, v in article2number_of_theorems_or_definitons.items():
+        key = re.sub(r"\.miz", "", k).upper()
+        number_of_theorems_and_definitions.append(float(v))
+        authority.append(float(article2authortiy[key]))
+        
+    s1 = pd.Series(number_of_theorems_and_definitions)
+    s2 = pd.Series(authority)
+
+    res = s1.corr(s2)
+
+    return res
+
 if __name__ == '__main__':
     res1 = calc_correlation_coefficient_number_of_referenced_and_authortiy_minus_pagerank()
     res2 = calc_correlation_coefficient_number_of_referenced_and_pagerank()
@@ -121,6 +175,11 @@ if __name__ == '__main__':
     res4 = calc_correlation_coefficient_number_of_labels_and_authortiy_minus_pagerank()
     res5 = calc_correlation_coefficient_number_of_labels_and_pagerank()
     res6 = calc_correlation_coefficient_number_of_labels_and_authortiy()
+
+    res7 = calc_correlation_coefficient_number_of_theorem_and_definiton_between_authoritiy_minus_pagerank()
+    res8 = calc_correlation_coefficient_number_of_theorem_and_definiton_between_pagerank()
+    res9 = calc_correlation_coefficient_number_of_theorem_and_definiton_between_authoritiy()
+
     cwd = os.getcwd()
     try:
         os.chdir("research_data/scatter_plots")
@@ -130,6 +189,9 @@ if __name__ == '__main__':
                     + "<number_of_referenced> - <authority>: " + str(res3) + "\n"
                     + "<number_of_labels> - <authority_minus_pagerank>: " + str(res4) + "\n"
                     + "<number_of_labels> - <pagerank>: " + str(res5) + "\n"
-                    + "<number_of_labels> - <authority>: " + str(res6) + "\n")
+                    + "<number_of_labels> - <authority>: " + str(res6) + "\n"
+                    + "<number_of_theorems_and_definitions> - <authority_minus_pagerank>: " + str(res7) + "\n"
+                    + "<number_of_theorems_and_definitions> - <pagerank>: " + str(res8) + "\n"
+                    + "<number_of_theorems_and_definitions> - <authority>: " + str(res9) + "\n")
     finally:
         os.chdir(cwd)
